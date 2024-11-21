91df9acfe33a29f074df2e9cc572a1a30a093756
Dan Ahern <danieljdahern@gmail.com>
2024-11-21 01:39:29 +0000

    Refactoring - rewrote sections of code to make program logic flow better and to let the program be more modular

--- web.py
+++ web.py
@@ -34,60 +34,98 @@ def parse_special_items(line):
 
     #elif ("GO TO" in temp_text or "GO" in temp_text):
         #data_entry['Action Type'] = "Travel"
-    
-def get_section(id):
 
-    class_string = "SectionContainer"+str(id)
+# Creates and returns the correct complete CSS class using inputted ID.
+def get_class_string(id):
+    return "SectionContainer"+str(id)
 
-    date = ""
-    day  = ""
-    
+# Uses created class string to isolate section container from the website.
+# Returns all rows held within that section for further processing.
+def get_section_container(class_string):
+
     section = soup.find("div", id=class_string)
     all_rows = section.find_all("tr")
-    del all_rows[0:3]
-    line = []
+    del all_rows[0:3] # Trims out the starting preamble from the list so we purely have tasks.
+    return all_rows
 
-    for row in all_rows:
-        row_data = row.find_all("td")
-        for entry in row_data:
-            parse_special_items(entry) 
+# Takes in the section container and breaks down each row into table data cells from which to extract data.
+def extract_cells(section_container):
+    for cell in section_container:
+        segment = cell.find_all("td")
+        date = format_date(segment[0].text)
+        day = format_day(segment[0].text)
+        day_tasks = segment[1].text.split(";")
+        night_tasks = None
 
-            data_entry = {'Date': date, 'Day': day, 'Time of Day': "Day", 'Action Type': "Action", 'Missable': "", 'Steps': []}
-            steps = []
-            entry_text = entry.text
-
-
-
-            if entry_text[0].isdigit():
-                date = entry_text[0:5] 
-                day = entry_text[5:8] 
-
-                data_entry['Date'] = date 
-                data_entry['Day'] = day
-            else:
-                if (len(diary) != 0):
-                   
-                    if (day == diary[len(diary)-1]['Day']) :
-                        data_entry['Time of Day'] = "Night"
-
-                steps.append(entry_text.split(";"))
-                data_entry['Steps'] = steps
-
-                data_entry['Action Type'] = get_action_type(entry_text.upper())
         
-            if len(steps) > 0:
-                diary.append(data_entry)
+        store_task_in_diary(build_task_package(date, day, "Day", day_tasks))
+
+        if len(segment) > 2:
+            night_tasks = segment[2].text.split(";")
+            store_task_in_diary(build_task_package(date, day, "Night", night_tasks))
+            
+        
+    return 
+
+def build_task_package(date, day, time_of_day, tasks):
+    task = {}
+    task['date'] = date
+    task['day'] = day
+    task['time_of_day'] = time_of_day
+    task['tasks'] = tasks
+
+    return task
+
+def store_task_in_diary(task):
+    diary.append(task)
 
 def get_action_type(input):
-    output = None
 
-    # Book items
+    # Identifies actions which involve reading a book.
     if "READ THE BOOK" in input:
         return "Read Book"
-
+    
+    # Identifies actions which involve travelling from one location to another.
     elif ("GO TO" in input or "GO" in input):
         return "Travel"
 
+def format_date(date):
+    return date[0:5]
+
+def format_day(day):
+    return day[5:8]
+    
+def test(row_data):
+    for row in row_data:
+        creeate_task_package(row)
+
+def creeate_task_package():
+    date = None
+    day  = None
+
+    data_entry = {'Date': date, 'Day': day, 'Time of Day': "Day", 'Action Type': "Action", 'Missable': "", 'Steps': []}
+    steps = []
+    entry_text = input.text
+
+    for row in input:
+        row_data = input.find_all("td", attrs={'style':'text-align:justify'})
+        for entry in row_data:
+            if entry_text[0].isdigit():
+                date = format_date(entry_text) 
+                day = format_day(entry_text)
+
+                data_entry['Date'] = date 
+                data_entry['Day'] = day
+            
+                if (len(diary) != 0):
+                    if (day == diary[len(diary)-1]['Day']) :
+                        data_entry['Time of Day'] = "Night"
+                steps.append(entry_text.split(";"))
+                data_entry['Steps'] = steps
+                data_entry['Action Type'] = get_action_type(entry_text.upper())
+            
+                diary.append(data_entry)
+        
 
 
     
@@ -118,15 +156,24 @@ def scrape_dates():
    # print(f"Total days found: {len(all_days)} Table: {all_rows[0]}")
     proceed = False
 
+def main():
+    # Construct Class String
+    class_string = get_class_string(2)
+    # Isolate section container for the selected month
+    section_container = get_section_container(class_string)
+    # Process Rows
+    extract_cells(section_container)
+
+
+
+    # Convert to pandas DataFrame for easier manipulation
+    df = pd.DataFrame(diary)
+    df.to_json("Export.json")
+    # Display the DataFrame
+    print(df)
+    proceed = False
 
 while(proceed):
    
-    scrape_dates()
-    egg = process_section(2, "april")
-    get_section(2)
+    main()
     proceed = False
-# Convert to pandas DataFrame for easier manipulation
-df = pd.DataFrame(diary)
-df.to_csv("Export.csv")
-# Display the DataFrame
-print(df)
